cccc4ca1da4a704223c5682c8ee4ad94486786c4
fuyingzhuang <fyz15731966959@163.com>
2023-12-10 18:07:27 +0800

    1. 代码沙箱接口的开发

--- src/main/java/com/ambition/fyzojcodesandbox/JavaCodeSandboxTemplate.java
+++ src/main/java/com/ambition/fyzojcodesandbox/JavaCodeSandboxTemplate.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Ambition
@@ -203,5 +204,6 @@ public abstract class JavaCodeSandboxTemplate implements CodeSandbox {
         executeCodeResponse.setStatus(2);
         executeCodeResponse.setJudgeInfo(new JudgeInfo());
         return executeCodeResponse;
+
     }
 }
--- src/main/java/com/ambition/fyzojcodesandbox/controller/MainController.java
+++ src/main/java/com/ambition/fyzojcodesandbox/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.ambition.fyzojcodesandbox.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ambition.fyzojcodesandbox.JavaNativeCodeSandbox;
+import com.ambition.fyzojcodesandbox.model.ExecuteCodeRequest;
+import com.ambition.fyzojcodesandbox.model.ExecuteCodeResponse;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Ambition
@@ -13,8 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/code/sandbox")
 public class MainController {
 
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
+
     @GetMapping("/health")
     public String getHealth() {
         return "ok";
     }
+
+
+    @Resource
+    private JavaNativeCodeSandbox javaNativeCodeSandbox;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    @PostMapping("/executeCode")
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                    HttpServletResponse response) {
+        // 基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("请求参数为空");
+        }
+        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
+    }
+
 }
